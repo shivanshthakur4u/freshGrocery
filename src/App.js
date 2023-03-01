2612a4cd871703e6e8467d49098ee16fee8537bc
Saurabh Singh <boyslike2play@gmail.com>
2023-03-01 16:06:07 +0530

    navbar completed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from 'react'
+import {Route, Routes, BrowserRouter} from 'react-router-dom'
+import Homepage from './Pages/Homepage'
+import Notfound from './Pages/Notfound'
+import './App.css'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Homepage/>}/>
+      <Route path='/home' element={<Homepage/>}/>
+      <Route path='*' element={<Notfound/>}/>
+    </Routes>
+  </BrowserRouter>
+  )
 }
 
-export default App;
+export default App
---
+++ src/Pages/Notfound.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Notfound = () => {
+  return (
+    <div>404notfound</div>
+  )
+}
+
+export default Notfound
