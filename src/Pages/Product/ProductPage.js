9b3542a6f2092327769f9d9c0284d6d5dfc3001d
Saurabh Singh <boyslike2play@gmail.com>
2023-03-15 11:51:54 +0530

    Product Page added

--- src/Pages/Product/ProductPage.js
+++ src/Pages/Product/ProductPage.js
@@ -4,174 +4,273 @@ import img1 from "../../Assets/Images/apple.png";
 import img2 from "../../Assets/Images/capsicum.png";
 import img3 from "../../Assets/Images/chillie.png";
 import Navbar from "../../Components/Navbar/Navbar";
-import '../../Pages/Product/ProductPage.css';
-import '../../../src/App.css';
-
+import "../../Pages/Product/ProductPage.css";
+import "../../../src/App.css";
+import Footer1 from "../../Components/Footer/Footer1";
+import Footer2 from "../../Components/Footer/Footer2";
 
 const ProductPage = () => {
-//   const { prodid } = useParams();
+  //   const { prodid } = useParams();
   const [imageset, setimageset] = React.useState(null);
   const [productdata, setproductdata] = React.useState([]);
   const [activeimg, setactiveimg] = React.useState({});
   const [count, setcount] = React.useState(1);
-
+  const [showreview, setShowreview] = React.useState(false);
 
   const getproductdatabyid = async () => {
     let temp = {
-        "Code": 200,
-        "Message": "Success",
-        "Data": [
+      Code: 200,
+      Message: "Success",
+      Data: [
+        {
+          ProductId: 1,
+          ProductName: "Product 1",
+          ProductDescription: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+          ProductImage: [
             {
-                "ProductId": 1,
-                "ProductName": "Product 1",
-                "ProductDescription": "Product 1 Description",
-                "ProductImage": [
-                    {
-                        id: 1,
-                        image: img1
-                    },
-                    {
-                        id: 2,
-                        image: img2
-                    },
-                    {
-                        id: 3,
-                        image: img3
-                    }
-                ],
-                "ProductCode": "P1",
-                "ProductCategory": "Category 1",
-                "ProductSubCategory": "Sub Category 1",
-                "ProductBrand": "Brand 1",
-                "ProductColor": "Color 1",
-                "ProductSize": "Size 1",
-                "ProductWeight": "Weight 1",
-                "ProductMaterial": "Material 1",
-                "ProductQuantity": 10,
-                "ProductUnit": "Unit 1",
-                "ProductPrice": 100,
-                "SalesPrice": 80,
-                "ProductDiscount": 20,
-                "ProductDiscountType": "Percentage",
-                "ProductTax": 20,
-                "ProductTaxType": "Percentage",
-                "ProductShippingCharge": 20,
-                "ProductShippingChargeType": "Percentage",
-                "ProductShippingTime": "1-2 days",
-                "ProductShippingTimeType": "Days",
-                "ProductShippingLocation": "Location 1",
-                "ProductShippingLocationType": "Country",
-                "ProductShippingReturnPolicy": "Return Policy 1",
-                "ProductShippingReturnPolicyType": "Days",
-                "ProductShippingReturnPolicyDescription": "Return Policy Description 1",
-                "ProductShippingReturnPolicyDescriptionType": "Days"
-            }
-        ]
-    }
+              id: 1,
+              image: img1,
+            },
+            {
+              id: 2,
+              image: img2,
+            },
+            {
+              id: 3,
+              image: img3,
+            },
+          ],
+          ProductCode: "P1",
+          ProductCategory: "Category 1",
+          ProductSubCategory: "Sub Category 1",
+          ProductBrand: "Brand 1",
+          ProductColor: "Color 1",
+          ProductSize: "Size 1",
+          ProductWeight: "Weight 1",
+          ProductMaterial: "Material 1",
+          ProductQuantity: 10,
+          ProductUnit: "Unit 1",
+          ProductPrice: 100,
+          SalesPrice: 80,
+          ProductDiscount: 20,
+          ProductDiscountType: "Percentage",
+          ProductTax: 20,
+          ProductTaxType: "Percentage",
+          ProductShippingCharge: 20,
+          ProductShippingChargeType: "Percentage",
+          ProductShippingTime: "1-2 days",
+          ProductShippingTimeType: "Days",
+          ProductShippingLocation: "Location 1",
+          ProductShippingLocationType: "Country",
+          ProductShippingReturnPolicy: "Return Policy 1",
+          ProductShippingReturnPolicyType: "Days",
+          ProductShippingReturnPolicyDescription: "Return Policy Description 1",
+          ProductShippingReturnPolicyDescriptionType: "Days",
+          ProductReviews: [
+            {
+              ReviewId: 1,
+              Name: "Saurabh Singh",
+              Email: "user@gmail",
+              Rating: 5,
+              Date: "2021-08-01",
+              Review:
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. ",
+            },
+            {
+              ReviewId: 2,
+              Name: "Saurabh Singh2",
+              Email: "user@gmail2",
+              Rating: 4,
+              Date: "2021-08-02",
+              Review:
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. ",
+            },
+            {
+              ReviewId: 3,
+              Name: "Saurabh Singh1",
+              Email: "user@gmail1",
+              Rating: 3,
+              Date: "2021-08-03",
+              Review:
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. ",
+            },
+          ],
+        },
+      ],
+    };
 
     if (temp.Code === 200) {
-        setimageset(temp.Data[0].ProductImage)
-        setproductdata(temp.Data[0])
-        setactiveimg(temp.Data[0].ProductImage[0])
+      setimageset(temp.Data[0].ProductImage);
+      setproductdata(temp.Data[0]);
+      setactiveimg(temp.Data[0].ProductImage[0]);
     }
-}
+  };
 
-
-useEffect(() => {
-    getproductdatabyid()
-}, [])
-return (
-    <div className='productpage'>
-        {/* <h1>Product id is - {prodid}</h1>
+  useEffect(() => {
+    getproductdatabyid();
+  }, []);
+  return (
+    <div className="productpage">
+      {/* <h1>Product id is - {prodid}</h1>
         <p>
             {JSON.stringify(productdata)}
         </p> */}
 
-        <Navbar />
+      <Navbar />
 
-        <div className='pc1'>
-            <Link to='/'>
-                <button className='goback'>
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-                        <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 19.5L8.25 12l7.5-7.5" />
-                    </svg>
-                    Go Back
-                </button>
-            </Link>
+      <div className="pc1">
+        <Link to="/">
+          <button className="goback">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-6 h-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M15.75 19.5L8.25 12l7.5-7.5"
+              />
+            </svg>
+            Go Back
+          </button>
+        </Link>
 
-
-            <div className='c11'>
-                <div className='imgset'>
-                    {
-                        imageset && imageset?.map((item,index)=>{
-                            return (
-                                <div className='imgsmall'
-                                    onClick={()=>{
-                                        setactiveimg(item)
-                                    }}
-                                >
-                                    <img src={item.image} alt="" 
-                                     className={
-                                        activeimg.id === item.id ? 'active' : ''
-                                     }
-                                    />
-                                </div>
-                            )
-                        })
-                    }
-                </div>
-                <div className='imgactive'>
-                    <img src={activeimg.image} alt="" />
-                </div>
-            </div>
-
-            <div className='c12'>
-                <h1 className='head1'>{productdata.ProductName}</h1>
-                <div className='c121'>
-                    <p className='price'>
-                        ${productdata.SalesPrice * count}
-                        <span>${productdata.ProductPrice * count}</span>
-                    </p>
-
-                    <div className='incrdecr'>
-                        <button
-                            onClick={() => {
-                                if (count > 1) {
-                                    setcount(count - 1)
-                                }
-                            }}
-                        >-</button>
-                        <p>{count}</p>
-                        <button
-                            onClick={() => {
-                                if (count < 10) {
-                                    setcount(count + 1)
-                                }
-                            }}
-                        >+</button>
-                    </div>
-                </div>
-
-                <div className='btncont'>
-                    <button
-                        onClick={() => {
-                            alert('Added to cart')
-                        }}
-                    >
-                        Add to Cart
-                    </button>
-                    <button
-                        onClick={() => {
-                            alert('Buy Now')
-                        }}
-                    >
-                        Buy Now
-                    </button>
-                </div>
-            </div>
+        <div className="c11">
+          <div className="imgset">
+            {imageset &&
+              imageset?.map((item, index) => {
+                return (
+                  <div
+                    className="imgsmall"
+                    onClick={() => {
+                      setactiveimg(item);
+                    }}
+                  >
+                    <img
+                      src={item.image}
+                      alt=""
+                      className={activeimg.id === item.id ? "active" : ""}
+                    />
+                  </div>
+                );
+              })}
+          </div>
+          <div className="imgactive">
+            <img src={activeimg.image} alt="" />
+          </div>
         </div>
+
+        <div className="c12">
+          <h1 className="head1">{productdata.ProductName}</h1>
+          <div className="c121">
+            <p className="price">
+              ${productdata.SalesPrice * count}
+              <span>${productdata.ProductPrice * count}</span>
+            </p>
+
+            <div className="incrdecr">
+              <button
+                onClick={() => {
+                  if (count > 1) {
+                    setcount(count - 1);
+                  }
+                }}
+              >
+                -
+              </button>
+              <p>{count}</p>
+              <button
+                onClick={() => {
+                  if (count < 10) {
+                    setcount(count + 1);
+                  }
+                }}
+              >
+                +
+              </button>
+            </div>
+          </div>
+
+          <div className="btncont">
+            <button
+              onClick={() => {
+                alert("Added to cart");
+              }}
+            >
+              Add to Cart
+            </button>
+            <button
+              onClick={() => {
+                alert("Buy Now");
+              }}
+            >
+              Buy Now
+            </button>
+          </div>
+        </div>
+      </div>
+
+      <div className="pc2">
+        {showreview ? (
+          <div className="tabs">
+            <button onClick={()=>{setShowreview(false)}} 
+            className="inactive">Description</button>
+            <button onClick={()=>{setShowreview(true)}}  className="active">Reviews</button>
+          </div>
+        ) : (
+          <div className="tabs">
+            <button onClick={()=>{setShowreview(false)}} className="active">Description</button>
+            <button onClick={()=>{setShowreview(true)}}  className="inactive">Reviews</button>
+          </div>
+        )}
+        {showreview ? (
+          <div className="reviewcont">
+            <form>
+              <div className="formgroup">
+                <label htmlFor="">Name</label> <input type={"text"} />
+              </div>
+
+              <div className="formgroup">
+                <label htmlFor="">Email</label> <input type={"email"} />
+              </div>
+
+              <div className="formgroup">
+                <label htmlFor="">My Review</label>{" "}
+                <textarea name="" id="" cols={"30"} rows="10"></textarea>
+              </div>
+              <button type="submit">Submit</button>
+            </form>
+          <div className="allreview">
+          <h1 className="head1">Product Reviews</h1>
+           {productdata.ProductReviews && productdata.ProductReviews.map((item,index)=>{
+            return(
+              <div className="review">
+              <div className="reviewhead">
+                <p className="name">{item.Name}</p>
+                <span className="date">{item.Date}</span>
+              </div>
+
+              <div className="reviewbody">
+                {item.Review}
+              </div>
+              </div>
+            )
+           })}
+          </div>
+          </div>
+        ) : (
+          <p className="desc">{productdata.ProductDescription}</p>
+        )}
+      </div>
+
+      <Footer1/>
+    <Footer2/>
     </div>
-)
+  );
 };
 
 export default ProductPage;
